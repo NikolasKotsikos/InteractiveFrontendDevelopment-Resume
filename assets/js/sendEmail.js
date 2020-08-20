9aebd6ea1988ad65388557b5748a1445e3fb4494
NikolasKotsikos <kotsikn@gmail.com>
2020-08-20 09:59:40 +0000

    Enabled email functionality on contact form

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,18 @@
+function sendEmail(contactForm) {
+    emailjs.send("gmail","rosie", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+            location.reload();
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+
+    return false;
+}
